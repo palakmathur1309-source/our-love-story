bbca8c258d448f05c0045253bd9ec2e285ee87c1
palakmathur1309-source <palakmathur1309@gmail.com>
2026-07-19 15:14:19 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -484,15 +484,3 @@ function createPetal(){
 setInterval(createPetal,800);
 
 
-const photos = [
-    "photo1.jpg",
-    "photo2.jpg",
-    "photo3.jpg",
-    "photo4.jpg",
-    "photo5.jpg",
-    "photo6.jpg",
-    "photo7.jpg",
-    "photo8.jpg",
-    "photo9.jpg",
-    "photo10.jpg"
-];
